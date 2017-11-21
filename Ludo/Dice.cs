01e0cd9afb79a26f089d9b2098564a5baff54762
Andreas Guldbrand <andr338a@edu.sde.dk>
2017-11-21 14:44:01 +0100

    dice first
    added basic dice

--- Ludo/Dice.cs
+++ Ludo/Dice.cs
@@ -3,9 +3,18 @@ namespace Ludo
 {
     public class Dice
     {
-        public Dice()
+        private int sides;
+        private Random rnd;
+
+        public Dice(int sides)
         {
-            
+            this.sides = sides;
+            this.rnd = new Random();
+        }
+
+        public int Throw(int sides)
+        {
+            return this.rnd.Next(sides+1);
         }
     }
 }
